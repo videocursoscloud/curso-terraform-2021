b7fbb457e6494fda537148555f546af334e034b6
miguel arranz <miguel@noteimporta.com>
2021-04-06 01:29:18 -0700

    changes

--- 05-modulos-parte-II/07-documentar-modulos/sqs/inputs.tf
+++ 05-modulos-parte-II/07-documentar-modulos/sqs/inputs.tf
@@ -1,2 +1,7 @@
-variable "queue_name"{}
-variable "delay_seconds"{}
+variable "queue_name"{
+  description = "Name of the queue to be created"
+}
+variable "delay_seconds"{
+  description = "delay seconds value. maps to delay seconds attribute on the queue. Delays the delivery to the queue"
+  default = "5"
+}
